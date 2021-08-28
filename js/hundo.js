e70e2eb2486d7d1d98ba1fbb0778ee8d48f8715a
Joel Ogunniran <joelnirano@gmail.com>
2021-08-28 22:13:22 +0100

    Debugging Test
    Using debugging to check null error that will occur if we misspelt id from html.

--- js/hundo.js
+++ js/hundo.js
@@ -1,22 +1,28 @@
+// alert("Hi from JS");
+
 //Get the value from the UI of the page/Interface
 
-
-alert("Hi from JS");
 //Start or Controller function(s)
 function getValues(){
    //get value from the page/screen
    let startValue = document.getElementById("startValue").value;
-
    let endValue = document.getElementById("endValue").value;
-
+   
+   alert("The Start Value is:" + startValue);
+   
    //We call generate Numbers
+   let numbers = generateNumbers();
 
-   // We call displayNumbers
+   //We call displayNumbers
+   displayNumbers();
+
 }
 
 //Generate numbers from the start value to the end value
 //Logic function(s)
 function generateNumbers(){
+   let numbers = [],
+   
 
 }
 
